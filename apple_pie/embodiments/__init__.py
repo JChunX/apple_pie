12ba0e45b8b3c8a7581f2a133f6916a8204ac86c
Jason X <realjchunx@gmail.com>
2025-03-20 21:08:37 -0700

    seeed init

--- apple_pie/embodiments/__init__.py
+++
@@ -1,3 +0,0 @@
-from .so100 import SO100Agent, SO100WristCam
-
-__all__ = ["SO100Agent", "SO100WristCam"]
--- apple_pie/tasks/__init__.py
+++
@@ -1,3 +0,0 @@
-from .peg_insertion_side_so100 import PegInsertionSideSO100Env
-
-__all__ = ["PegInsertionSideSO100Env"]
